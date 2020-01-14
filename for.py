947f8eceece2ce42c212ed073220cb534f20c083
giancarlopernudisegura <pernudig@gmail.com>
2020-01-13 23:06:27 -0700

    c style for loops

--- for.py
+++ for.py
@@ -5,3 +5,15 @@ print("Hello World")
 print("Hello World")
 
 # How can I acheive the same effect with less lines?
+
+
+# Does this ever stop?
+while True:
+    print("yeet")
+
+# What is the equivalent of?
+# for (int i = 0; i < 5; i++)
+
+# for (int i = -3; i < 4; i++)
+
+# for (int i = 0; i < 10; i += 2)
